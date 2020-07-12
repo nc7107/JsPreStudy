fa35a1838439cb488bd4f8c073ba042bf23fa60b
nc7107 <52659185+nc7107@users.noreply.github.com>
2020-07-12 13:13:27 +0900

    Update clock
    -loadName()

---
+++ greeting.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".js-form"),
+  input = document.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+function init() {
+  loadName();
+}
+
+init();
